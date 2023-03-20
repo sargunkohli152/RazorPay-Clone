0fabfb087005d260d206d99e5831b3d6fe70d7ab
Sargun <sargun.kohli152@gmail.com>
2023-03-20 22:23:54 +0530

    my first  commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      heading: ['Poppins', 'sans-serif'],
+      para: ['Bai jamjuree', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
